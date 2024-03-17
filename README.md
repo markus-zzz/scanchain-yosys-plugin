b1f578f4b6c2711f675ac65424b1a6129055cf6e
Markus Lavin <markus.lavin@gmail.com>
2024-03-17 21:30:43 +0100

    [WIP] Populating repository

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+git clone https://github.com/YosysHQ/yosys.git
+cd yosys/
+make config-clang
+make -j8
+PREFIX=/home/markus/work/install/ make install
+```
---
+++ cntr.v
@@ -0,0 +1,14 @@
+module counter(input clk, input rst, output [7:0] result);
+
+  reg [7:0] cntr;
+
+  assign result = cntr;
+
+  always @(posedge clk) begin
+    if (rst)
+      cntr <= 0;
+    else
+      cntr <= cntr + 1;
+  end
+
+endmodule
